838acb7e61a9dc62b7e239ef02be6996cadb48aa
abhi pawar <aspawar1727@gmail.com>
2025-02-07 07:26:52 +0530

    Add proxy configuration to vite.config.js for API calls

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://107.21.85.231:8292',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   build: {
     outDir: 'dist' // Ensure Vercel knows where to find the built files
   },
-})
+});
